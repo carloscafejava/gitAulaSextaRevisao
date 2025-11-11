0d326789443620bed9f6af50687b0caaf6520d97
Larissa Pinheiro <116392603+LariGranger@users.noreply.github.com>
2025-11-11 00:27:32 +0000

    atv Spring

--- atividadebancodedados/src/main/java/com/javaspringatt/atividadebancodedados/controller/AttBancoController.java
+++ atividadebancodedados/src/main/java/com/javaspringatt/atividadebancodedados/controller/AttBancoController.java
@@ -3,8 +3,11 @@ package com.javaspringatt.atividadebancodedados.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -20,15 +23,40 @@ public ResponseEntity<AttBancoModel> listarTodos(){
     List<AttBancoModel> produtos = attService.listarTodos();
     return ResponseEntity.ok(produtos);
 }
-
+//tras algo do banco, busca informação através do ID
+//@PathVariable - URL
+//@RequestBody - página
 @GetMapping("/{id}")
 public ResponseEntity<AttBancoModel> buscarPorId(@PathVariable int id){
-    
+    AttBancoModel produto = attService.buscarPorId(id);
+        if(produto != null){
+            return ResponseEntity.ok(produto);
+        }
+        return ResponseEntity.notFound().build();
+}
+
+@PostMapping
+public ResponseEntity<AttBancoModel> adicionarProduto(@RequestBody AttBancoModel produto){
+    AttBancoModel produtoSalvo = attService.adicionarProduto(produto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(produtoSalvo);
 }
 
 @PutMapping("/{id}")
 public ResponseEntity<> atualizarProduto(@PathVariable int id,  @RequestBody produto){
+    AttBancoModel produtoAtualizado = attService.atualizarProduto(id, produto);
+        if(produtoAtualizado != null){
+            return ResponseEntity.notFound().build();
+        }
+@DeleteMapping("/{id}")
+public ResponseEntity<void> deletarProduto(@PathVariable in id){
+    AttBancoModel produto = attService.buscarPorId(id);
+    if(produto != null){
+        attService.deletarProduto(id);
+        return ResponseEntity.noContent().build();
+    }
 
+    return ResponseEntity.notFound().build();
+}
 }
 
 
